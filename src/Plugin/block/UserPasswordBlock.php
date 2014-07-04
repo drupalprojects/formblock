418e0a5e8b1397c802700be839ecfd408d3c0226
Michael Prasuhn <mike@mikeyp.net>
2014-07-03 17:59:00 -0700

    Updating user password block

--- src/Plugin/block/UserPasswordBlock.php
+++ src/Plugin/block/UserPasswordBlock.php
@@ -2,10 +2,11 @@
 
 namespace Drupal\formblock\Plugin\Block;
 
-use Drupal;
 use Drupal\block\BlockBase;
 use Drupal\Component\Annotation\Block;
-use Drupal\Core\Annotation\Translation;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Form\FormBuilderInterface;
 
 /**
  * Provides a block for the password reset form.
@@ -19,14 +20,53 @@ use Drupal\Core\Annotation\Translation;
  * Note that we set module to contact so that blocks will be disabled correctly
  * when the module is disabled.
  */
-class UserPasswordBlock extends BlockBase {
+class UserPasswordBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The form builder.
+   *
+   * @var \Drupal\Core\Form\FormBuilderInterface.
+   */
+  protected $formBuilder;
+
+  /**
+   * Constructs a new NodeFormBlock plugin
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Form\FormBuilderInterface $formBuilder
+   *   The form builder.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, FormBuilderInterface $formBuilder) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->formBuilder = $formBuilder;
+  }
+
+
+  /**
+   * {@inherticdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('form_builder')
+    );
+  }
+
+
   /**
    * Implements \Drupal\block\BlockBase::build().
    */
   public function build() {
     $build = array();
 
-    $build['form'] = \Drupal::formBuilder()->getForm('Drupal\user\Form\UserPasswordForm');
+    $build['form'] = $this->formBuilder->getForm('Drupal\user\Form\UserPasswordForm');
 
     return $build;
   }
